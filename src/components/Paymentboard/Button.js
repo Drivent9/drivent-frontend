590d6cb654423e8636ba19d76f9bc3d4d92a1db4
breno-aredes <breno_mg10@hotmail.com>
2023-05-10 19:18:31 -0300

    fix: Buttom

--- src/components/Paymentboard/Button.js
+++ src/components/Paymentboard/Button.js
@@ -10,6 +10,5 @@ const StyledMuiButton = styled(MuiButton)`
   margin-top: 8px !important;
 `;
 
-// este modelo de botão é o mesmo do forms
-//definir a props, o children deveria ser "finalizar pagamento"
-//se ele é um componente usado em mais de um lugar, deveria ter uma pasta propria? ou ser realocado? para evitar repetição de codigo?
+// APENAS PARA TESTES. O BOTÃO ESTA FEITO NO FORMS
+//definir a props e children para o botão original, entender o codigo do botão.
