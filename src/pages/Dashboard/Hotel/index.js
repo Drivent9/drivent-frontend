5ce26c3c919193b81f0f2bb3050c69b72a97cc44
taianaazevedo <taianazev@gmail.com>
2023-05-11 14:55:36 -0300

    feat: show message when ticket is remote or not paid

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,46 @@
+import styled from 'styled-components';
+import Typography from '@material-ui/core/Typography';
+
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  return (
+    <>
+      <StyledTypography variant="h4">Escolha de hotel e quarto</StyledTypography>
+      <MessageWheinTicketIsRemote>
+        Sua modalidade de ingresso não inclui hospedagem
+        <br />
+        Prossiga para a escolha de atividades
+      </MessageWheinTicketIsRemote>
+      <MessageWhenTicketIsNotPaid>
+        Você precisa ter confirmado pagamento antes
+        <br />
+        de fazer a escolha de hospedagem
+      </MessageWhenTicketIsNotPaid>
+    </>
+  );
 }
+
+const StyledTypography = styled(Typography)`
+  margin-bottom: 20px !important;
+`;
+
+const MessageWheinTicketIsRemote = styled.p`
+  font-family: Roboto;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: #8e8e8e;
+  margin-top: 240px;
+`;
+
+const MessageWhenTicketIsNotPaid = styled.p`
+  font-family: Roboto;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: 0em;
+  text-align: center;
+  color: #8e8e8e;
+  margin-top: 240px;
+`;
