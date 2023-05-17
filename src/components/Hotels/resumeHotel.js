4f6089a66e9ebefb4af8110d91e4f777e8e15852
breno-aredes <breno_mg10@hotmail.com>
2023-05-17 11:13:01 -0300

    feat: resume component now does get in hotelsRooms

--- src/components/Hotels/resumeHotel.js
+++ src/components/Hotels/resumeHotel.js
@@ -1,15 +1,19 @@
+import useHotelRooms from '../../hooks/api/useHotelsRooms';
 import Button from '../Form/Button';
 import { Title } from '../Paymentboard/styled';
 
 export default function ResumeHotel({ bookingUser }) {
-  if (!bookingUser) {
+  const { hotelsRooms } = useHotelRooms(bookingUser.Room.hotelId);
+
+  if (!bookingUser || !hotelsRooms) {
     return <></>;
   }
-
+  console.log(hotelsRooms);
   return (
     <>
       <Title>Você já escolheu seu quarto:</Title>
       <p>{bookingUser.Room.id} (teste para ver o Room Id)</p>
+      <p>{hotelsRooms.name} (teste para ver se hotelsRooms esta funcionando)</p>
       <Button>TROCAR DE QUARTO</Button>
     </>
   );
