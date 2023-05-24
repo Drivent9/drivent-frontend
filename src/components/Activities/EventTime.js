4bea18b8ce840afcbdd01c987d51c2572aba8320
breno-aredes <breno_mg10@hotmail.com>
2023-05-24 14:10:35 -0300

    feat: activities hours layout base

--- src/components/Activities/EventTime.js
+++ src/components/Activities/EventTime.js
@@ -5,15 +5,20 @@ export default function EventTime() {
   return (
     <Container>
       <AuditoriumContainer>
-        <Title>Auditório Principal</Title>
+        <Title>Auditório do banco </Title>
+        <TimeContainer>
+          <HourActivities>
+            <h1>Minecraft: montando o PC ideal</h1>
+            <h2>09:00 - 10:00</h2>
+          </HourActivities>
+        </TimeContainer>
+      </AuditoriumContainer>
+      <AuditoriumContainer>
+        <Title>Auditório do banco</Title>
         <TimeContainer></TimeContainer>
       </AuditoriumContainer>
       <AuditoriumContainer>
-        <Title>Auditório Principal</Title>
-        <TimeContainer></TimeContainer>
-      </AuditoriumContainer>
-      <AuditoriumContainer>
-        <Title>Auditório Principal</Title>
+        <Title>Auditório do banco</Title>
         <TimeContainer></TimeContainer>
       </AuditoriumContainer>
     </Container>
@@ -38,4 +43,27 @@ const TimeContainer = styled.div`
   width: 280px;
   height: 390px;
   border: 1px solid #cecece;
+  padding: 10px;
+`;
+
+const HourActivities = styled.div`
+  width: 260px;
+  height: 80px;
+  background-color: #f1f1f1;
+  border-radius: 5px;
+  padding: 10px;
+  h1 {
+    color: #343434;
+    font-family: 'Roboto', sans-serif;
+    font-size: 12px;
+    font-weight: 700;
+    line-height: 14px;
+    margin-bottom: 6px;
+  }
+  h2 {
+    font-family: 'Roboto', sans-serif;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 14px;
+  }
 `;
