54dd49d25ab53dc35e34041fc0e08e1aa820819e
taianaazevedo <taianazev@gmail.com>
2023-05-25 11:05:21 -0300

    feat: event days

--- src/components/Activities/EventDay.js
+++ src/components/Activities/EventDay.js
@@ -1,18 +1,41 @@
 import styled from 'styled-components';
-import Button from '../Form/Button';
 import { Title } from '../Paymentboard/styled';
 
-export default function EventDay() {
+export default function EventDay({ dates }) {
   return (
     <>
       <Title>Primeiro, filtre pelo dia do evento: </Title>
-      <ButtonDay>data do banco</ButtonDay>
-      <ButtonDay>data do banco</ButtonDay>
-      <ButtonDay>data do banco</ButtonDay>
+      <Days>
+        {dates.map((date) => {
+          return (
+            <>
+              <ButtonDay>{date}</ButtonDay>
+            </>
+          );
+        })}
+      </Days>
     </>
   );
 }
 
-const ButtonDay = styled(Button)`
-  margin-right: 17px !important;
+const ButtonDay = styled.button`
+  width: 140px;
+  height: 37px;
+  margin-right: 17px;
+  margin-top: 17px;
+  background: #e0e0e0;
+  box-shadow: 0px 2px 7px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  border: none;
+  font-size: 14px;
+  font-family: 'Roboto';
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Days = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
 `;
--- src/components/Activities/Index.js
+++ src/components/Activities/Index.js
@@ -2,13 +2,44 @@ import { Typography } from '@material-ui/core';
 import styled from 'styled-components';
 import EventDay from './EventDay';
 import EventTime from './EventTime';
+import { useEffect, useState } from 'react';
+import { getEventInfo } from '../../services/eventApi';
+import { eachDayOfInterval, format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
 export default function ActiviesIndexComponents() {
+  const [dates, setDates] = useState([]);
+  const [error, setError] = useState('');
+
+  async function getDays() {
+    try {
+      const response = await getEventInfo();
+      const { startsAt, endsAt } = response;
+
+      const startDate = new Date(startsAt);
+      const endDate = new Date(endsAt);
+
+      const days = eachDayOfInterval({ start: startDate, end: endDate });
+      const formattedDays = days.map((day) => format(day, 'EEE, dd/MM', { locale: ptBR }));
+
+      setDates(formattedDays);
+    } catch (error) {
+      setError(error);
+    }
+  }
+
+  useEffect(() => {
+    getDays();
+  }, []);
+
+  if (error) {
+    return <p>Something went wrong. Please, try again.</p>;
+  }
   return (
     <>
       <StyledTypography variant="h4">Escolha de atividades</StyledTypography>
 
-      <EventDay />
+      <EventDay dates={dates} />
 
       <EventTime></EventTime>
     </>
